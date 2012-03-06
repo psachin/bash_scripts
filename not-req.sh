c0dcee91cca7d0591b7244979ffdff2d165c1423
psachin <iclcoolster@gmail.com>
2012-03-06 15:23:58 +0530

    Bash script to remove not-require files recursively

--- not-req.sh
+++ not-req.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 # removing *.not-required files from all directories/sub-directories
 
-FILENAME='dirty_file.txt'
+
+FILENAME=dirty_file.txt
 
 find -type f -iname $FILENAME -exec rm -v {} \;
 
