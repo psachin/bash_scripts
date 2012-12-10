09382ea592834c7ac42d66bc465ae268ab63dfe2
psachin <iclcoolster@gmail.com>
2012-12-10 15:34:20 +0530

    Subject: some more handy scripts added
    Description:
    
       bashGitbranch.sh - change bash proxy according to git branch
       ch-mount.sh - chrooting the rootfs on SDcard
       chk_ph.sh - tarnsfer files to another lab

---
+++ ch-mount.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# 
+
+
+function mnt() {
+    echo "MOUNTING"
+    sudo mount -t proc /proc ${2}proc
+    sudo mount -t sysfs /sys ${2}sys
+    sudo mount -o bind /dev ${2}dev
+    sudo mount -o bind /dev/pts ${2}dev/pts		
+    sudo chroot ${2}
+}
+
+function umnt() {
+    echo "UNMOUNTING"
+    sudo umount ${2}proc
+    sudo umount ${2}sys
+    sudo umount ${2}dev/pts
+    sudo umount ${2}dev
+
+}
+
+
+if [ $1 == '-m' ] && [ -n "$2" ];
+then
+    mnt $1 $2
+elif [ $1 == '-u' ] && [ -n "$2" ];
+then
+    umnt $1 $2
+else
+    echo ""
+    echo "Either 1'st, 2'nd or both parameters are missing"
+    echo ""
+    echo "first parameter can be one of these: -m OR -u"
+    echo "second parameter is the name of rootfs directory(with trailing '/')"
+    echo ""
+    echo 1st parameter : ${1}
+    echo 2nd parameter : ${2}
+fi
+
+
---
+++ chk_ph.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ZIP_FILE="c-cpp.zip"
+rm -vf online_pc.txt
+rm -vf offline_pc.txt
+
+if [ -f ${ZIP_FILE} ];
+then
+    echo "extracting ${ZIP_FILE} .. please wait"
+    unzip $ZIP_FILE
+else
+    echo "file ${ZIP_FILE} does not exist"
+fi
+
+for i in 10.112.16.{01..50}
+do 
+    ping -c 1 $i 
+    if [ $? -eq 0 ];
+	then
+	echo "$i is LIVE" >> online_pc.txt
+	IP=$(echo ${i} | cut -f 4 -d .)
+	echo ""
+	echo "password is same as username i.e physics${IP}"
+	echo "else try: student123 "
+	scp -v python physics${IP}@${$i}:~/Desktop/
+    else
+	echo "$i" is DOWN >> offline_pc.txt
+    fi
+done
+
