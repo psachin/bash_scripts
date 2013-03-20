8cdfad7e60adddd1df33255e7e42f4623fd7469e
psachin <iclcoolster@gmail.com>
2013-03-20 12:20:56 +0530

    Subject: change in `parse_git_dirty` function
    Description:
    
    1) latest git version 1.8.1.5 give status as:
    
    `nothing to commit, working directory clean` when the repo it clean,
    
    where as git version 1.7.9.5 gave
    
    `nothing to commit (working directory clean)`
    
    2) added zte_fetch.sh to pull photos from my phone

--- bashGitbranch.sh
+++ bashGitbranch.sh
@@ -2,7 +2,7 @@
 # ref: https://gist.github.com/31631
 
 function parse_git_dirty {
-    [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+    [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo "*"
 }
 
 function parse_git_branch {
---
+++ zte_fetch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# path inside phone to fetch files from
+PHONE_PATH='/sdcard/DCIM/Camera/'
+# path where to save file(s)
+DESTINATION="${HOME}/Pictures/zte/"
+
+
+PICS=$(adb shell ls ${PHONE_PATH})
+for image in ${PICS}; 
+do
+    #echo ${image}
+    # remove trailing '\r' if any
+    IMAGE=$(echo ${image} | tr -d '\r')
+    adb pull ${PHONE_PATH}${IMAGE}
+    rsync \
+	--verbose \
+	--checksum \
+	--recursive \
+	--update \
+	--times \
+	--human-readable \
+	--progress \
+	--remove-source-files \
+	${IMAGE} \
+	${DESTINATION}
+done 
+
+
+
