01e4236ec24420406fe90cda46c07d0bff56effc
psachin <iclcoolster@gmail.com>
2013-07-07 15:41:35 +0530

    Subject: if build.xml not exist, create it
    Description:
    
    1) fixed minor bug: /bin/ --> bin/
    2) fixed if-statement

--- start_app.sh
+++ start_app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # startup script for android project
 
-# first arg
+# first arg(optional)
 # rm, for deleting bin/ and gen/
 # update, to update project
 
 # vars
-PKG_NAME="com.vogella.android.userinterface.undo"
+PKG_NAME="com.mkyong.android"
 APK_NAME="MainActivity-debug.apk"
 
 # remove /bin and gen/ if first argument passed is 'rm'
@@ -14,7 +14,7 @@ function rmbingen() {
     if [ "$1" == "rm" ];
     then 
         # uncomment below 2 line to remove bin/ and gen/
-	if [ -d "bin/" || -d "gen/" ];
+	if [ -d "bin/" ] || [ -d "gen/" ];
 	then
 	    rm -rvf bin/
 	    rm -rvf gen/
@@ -29,24 +29,34 @@ function rmbingen() {
 rmbingen $1
 
 # remote old bin/ and gen/ and update the project
-if [ "$1" == "update" ];
-then
-    echo "creating ant project.."
-    rmbingen rm
-    # this will update a repo for `ant` to build an apk
-    android update project --path . --target android-17 --subprojects
-else
-    echo "not updating the project"
-fi
+function update_project() {
+    if [ "$1" == "update" ];
+    then
+	echo "creating ant project.."
+	rmbingen rm
+        # this will update a repo for `ant` to build an apk
+	android update project --path . --target android-17 --subprojects
+    else
+	echo "not updating the project"
+    fi
+}
+# execute function update_project
+update_project $1
 
 # uninstall apk
 adb uninstall ${PKG_NAME}
 
 # build apk in debug mode
 echo "Building APK.."
-ant debug
+# if 'build.xml' does not exist, create it.
+if [ -f "build.xml" ];
+then
+    ant debug
+else
+    update_project update
+fi
 
-if [ -f /bin/${APK_NAME} ];
+if [ -f bin/${APK_NAME} ];
 then
     # install apk
     echo "Installing APK.."
