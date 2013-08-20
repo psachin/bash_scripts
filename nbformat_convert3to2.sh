64ad1f605f1bdf5512a2db55bc4c2c7200d786e7
psachin <iclcoolster@gmail.com>
2013-08-20 20:02:08 +0530

    Subject: modified rm_space.sh to not to rename itself.
    Description:
    
    implemented IFS in nbformat_convert3to2.sh

--- nbformat_convert3to2.sh
+++ nbformat_convert3to2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
 # find all .ipynb(ipython notebook) files in present directoryand
 # convert nbformat from 3 to 2
 
 # USAGE: ./nbformat_convert3to2.sh
 
+NEW_IFS=$IFS			# save previous Internal Field Seperator
+IFS=$(echo -en "\n\b\:\,")	# set new IFS
+
+# replace string
 FILE_LIST=$(find . -type f -iname "*.ipynb")
 
 STRING='"nbformat": 3,'
@@ -12,7 +15,9 @@ REPLACE_STRING='"nbformat": 2,'
 #echo $STRING
 for FILE in ${FILE_LIST}
 do
-    # echo $FILE
-    sed -i 's/'"${STRING}"'/'"${REPLACE_STRING}"'/g' $FILE
+    #echo ${FILE}
+    sed -i 's/'"${STRING}"'/'"${REPLACE_STRING}"'/g' ${FILE}
 done
+
+IFS=$NEW_IFS			# restore old IFS
 
--- rm_space.sh
+++ rm_space.sh
@@ -3,8 +3,17 @@
 
 for f in *
 do
-    new_name=$(echo $f | tr " " "_" | sed 's/_//g')
-    mv -v "$f" "$new_name"
-done
+    new_name=$(echo $f | tr " " "_" | sed 's/_//g') # remote SPACE 
 
+    if [ "${f}" != "${new_name}" ]; # should not unnecessarily rename
+				    # files with same names
+    then
+    	if [ "./${f}" == "$0" ]; # should not try rename itself
+    	then
+    	    echo ""		
+    	else
+    	    mv -v "$f" "$new_name"
+    	fi
+    fi
+done
 
