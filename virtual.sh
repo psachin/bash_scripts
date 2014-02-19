096ad076ea4ed48a3005f9af6f19f54198540012
psachin <iclcoolster@gmail.com>
2014-02-19 11:08:39 +0530

    Subject: `setv` with more help and listing of all virtual environments.
    Description:

--- virtual.sh
+++ virtual.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Set virtual environment
 
+# Configure:
+# Set the `VIRTUAL_DIR_PATH` variable value to valid PATH to your virtual envs
+#
 # Usage:
 #
 # Added this file to your .bashrc or any local rc
@@ -12,27 +15,35 @@
 # For example:
 # $ setv rango
 #
+# To list all your virtual environments:
+# $ setv -l
+#
 # To deactivate type:
 # $ deactivate
 #
-
 
 # Path to your virtual environment directories
 VIRTUAL_DIR_PATH="/home/sachin/virtualenvs/"
 
 function setv() {
-    # Check if the virtual directory exists in PATH
-    if [ -d ${VIRTUAL_DIR_PATH}${1} ];
+    if [ ${1} == "-l" ];
+    then
+       echo "List of virtual environments you have under ${VIRTUAL_DIR_PATH}:"
+       echo " "
+       LIST_OF_VIRTUALENVS=$(ls -1 ${VIRTUAL_DIR_PATH})
+       for virt in ${LIST_OF_VIRTUALENVS}
+       do
+	   echo ${virt}
+       done
+       # Check if the virtual directory exists in PATH
+    elif [ -d ${VIRTUAL_DIR_PATH}${1} ];
     then
 	# Activate the virtual environment
 	source ${VIRTUAL_DIR_PATH}${1}/bin/activate
     else
 	# Else throw an error message
 	echo "Sorry, you don't have any virtual environment with that name"
+	echo "To list all virtual environments, type:"
+	echo "setv -l"
     fi
 }
-
-
-
-
-
