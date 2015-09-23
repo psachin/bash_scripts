ee3d6d6159856cb46ccea2ca93f0c8c8072b0a25
sachin <sachin.p@plancess.com>
2015-09-23 20:12:01 +0530

    Subject: Prevent globbing and word splitting

--- rm_space.sh
+++ rm_space.sh
@@ -4,7 +4,7 @@
 
 for f in *
 do
-    new_name=$(echo $f | tr " " "_" | sed 's/_//g') # remote SPACE
+    new_name=$(echo "$f" | tr " " "_" | sed 's/_//g') # remote SPACE
 
     if [ "${f}" != "${new_name}" ]; # should not unnecessarily rename
 				    # files with same names
