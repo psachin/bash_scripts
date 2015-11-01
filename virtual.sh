e8468daecdca0983a4a2991c481f9f11b2858de1
sachin <iclcoolster@gmail.com>
2015-11-02 01:01:16 +0530

    virtual.sh
    TODO:
    - Regressive test

--- virtual.sh
+++ virtual.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Set virtual environment:: for python virtualenv
-
+# Author: Sachin <iclcoolster@gmail.com>
+# Source: https://github.com/psachin/bash_scripts/virtual.sh
+#
+# License: GNU/GPL v3
+#
 # Configure:
 # Set `VIRTUAL_DIR_PATH` value to PATH to your virtual environments
 # directory. By default it is set to '~/virtualenvs/'
@@ -16,29 +20,36 @@
 # $ setv rango
 #
 # or type:
-# setv [TAB] [TAB]
-
+# setv [TAB] [TAB]  (to list all virtual envs)
+#
 # To list all your virtual environments:
 # $ setv -l
+#
+# To create new virtual environments:
+# $ setv -n new_virtualenv_name
+#
+# To delete new virtual environments:
+# $ setv -d existing_virtualenv_name
 #
 # To deactivate, type:
 # $ deactivate
 
-# TODO:
-# - Create virtualenv with command something like: setv new_virt (if does not exists)
+# TODO (Testing):
+# - Create virtualenv with command something like: setv -n new_virt_name
 # - Delete virtualenv with command something like: setv -d old_virt_name
 
 # ChangeLog :
 # Fri Apr 25 12:41:40 IST 2014: TAB completion
+# Mon Nov 02 12:51:54 IST 2015: Create/delete virtual environment
 
-# Path to your virtual environment directories
+# Path to virtual environment directory
 VIRTUAL_DIR_PATH="$HOME/virtualenvs/"
 
 function _setvcomplete_()
 {
     # bash-completion
-    local cmd="${1##*/}" # to handle command(s) that's beain
-                         # executed. Not necessary here as 'setv' is
+    local cmd="${1##*/}" # to handle command(s).
+                         # Not necessary here as 'setv' is
                          # the only command
     local word=${COMP_WORDS[COMP_CWORD]} # Words thats being completed
     local xpat='${word}'		 # Filter pattern. Include
@@ -55,7 +66,47 @@ function _setv_help_() {
     # Echo help
     echo "Help: "
     echo -e "setv [-l] \t\t\t to list all virtual envs."
-    echo -e "setv [virtual env name] \t to set virtual env."
+    echo -e "setv [Virtual_env_name] \t to set virtual env."
+    echo -e "setv -n [New virtual_env_name] \t to create virtual env."
+    echo -e "setv -d [virtual_env_name] \t to delete existing virtual env."
+}
+
+function _setv_create()
+{
+    if [ -z ${1} ];
+    then
+	echo "You need to pass virtual environment name"
+	_setv_help_
+    else
+	echo "Creating new virtual environment with the name: $1"
+	virtualenv -p $(which python) ${VIRTUAL_DIR_PATH}${1}
+    fi
+}
+
+function _setv_delete()
+{
+    # Need to refactor
+    if [ -z ${1} ];
+    then
+	echo "You need to pass virtual environment name"
+	_setv_help_
+    else
+	if [ -d ${VIRTUAL_DIR_PATH}${1} ];
+	then
+	    rm -rvf ${VIRTUAL_DIR_PATH}${1}
+	else
+	    echo "No virtual environment with name: ${1}"
+	fi
+    fi
+}
+
+function _setv_list() {
+    echo -e "List of virtual environments you have under ${VIRTUAL_DIR_PATH}:\n"
+    LIST_OF_VIRTUALENVS=$(ls -1 ${VIRTUAL_DIR_PATH})
+    for virt in ${LIST_OF_VIRTUALENVS}
+    do
+	echo ${virt}
+    done
 }
 
 function setv() {
@@ -64,24 +115,27 @@ function setv() {
     then
 	_setv_help_
     else
-	if [ ${1} == "-l" ];
-	then
-	    echo -e "List of virtual environments you have under ${VIRTUAL_DIR_PATH}:\n"
-	    LIST_OF_VIRTUALENVS=$(ls -1 ${VIRTUAL_DIR_PATH})
-	    for virt in ${LIST_OF_VIRTUALENVS}
-	    do
-		echo ${virt}
-	    done
-	    # Check if the virtual directory exists in PATH
-	elif [ -d ${VIRTUAL_DIR_PATH}${1} ];
-	then
-	    # Activate the virtual environment
-	    source ${VIRTUAL_DIR_PATH}${1}/bin/activate
-	else
-	    # Else throw an error message
-	    echo "Sorry, you don't have any virtual environment with that name"
-	    _setv_help_
-	fi
+	case ${1} in
+	    -n) _setv_create ${2}
+		;;
+
+	    -d) _setv_delete ${2}
+		;;
+
+	    -l) _setv_list
+		;;
+
+	    *) if [ -d ${VIRTUAL_DIR_PATH}${1} ];
+	       then
+		   # Activate the virtual environment
+		   source ${VIRTUAL_DIR_PATH}${1}/bin/activate
+	       else
+		   # Else throw an error message
+		   echo "Sorry, you don't have any virtual environment with that name"
+		   _setv_help_
+	       fi
+	       ;;
+	esac
     fi
 }
 
